b24f0d0a11169d5a54942d2e6d14858afdffdd8e
Georg Wille <georg.wille@mailbox.org>
2021-12-01 12:39:39 +0100

    added day 1

---
+++ aoc_01a.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+n = np.loadtxt('input_01.txt')
+
+def increasecount(field, k):
+    d = field[k:] - field[:-k]
+    count = np.argwhere(d > 0)
+    return count.shape[0]
+
+
+print(increasecount(n, 1))
