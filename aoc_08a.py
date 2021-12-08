075b1b4c08e56d3fe67bb0ac8c43e2992af065ae
Georg Wille <georg.wille@mailbox.org>
2021-12-08 18:19:35 +0100

    tightened 8a a bit

--- aoc_08a.py
+++ aoc_08a.py
@@ -1,17 +1,10 @@
 fin = open('input_08.txt')
 
-output = []
-
-for line in fin:
-    line = line.strip()
-    output.append(line.split(' | ')[1].split(' '))
-
 counter1478 = 0
 
-for line in output:
-    for entry in line:
+for line in fin:
+    for entry in line.strip().split(' | ')[1].split(' '):
         if len(entry) in [2,4,3,7]:
             counter1478 += 1
 
 print(counter1478)
-
